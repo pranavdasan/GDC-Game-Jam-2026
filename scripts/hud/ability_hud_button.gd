77eda648893364aa524ebfcfc094458d5b1a365a
cameron <cjbseattle@gmail.com>
2026-04-08 09:20:35 -0700

    Adds timer display for cooldown

--- scripts/hud/ability_hud_button.gd
+++ scripts/hud/ability_hud_button.gd
@@ -5,10 +5,13 @@ extends Button
 
 func _process(delta: float) -> void:
 	# sets cooldown bar to proportional cooldown remaining
+	# sets the text of this button to also show time remaining
 	if cooldown_timer.time_left != 0.0:
 		cooldown_bar.value = cooldown_timer.time_left / cooldown_timer.wait_time * 100
+		text = "%.2f" % cooldown_timer.time_left
 	else:
 		cooldown_bar.value = 0.0
+		text = ""
 
 # starts the cooldown timer based on ability cooldown
 func start_cooldown(ability_id : int):	
